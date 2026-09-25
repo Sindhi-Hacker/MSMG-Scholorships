422a769a6a740ab2fdbe121bdf8e8b2a97ea9107
Sindhi Hacker <sindhihacker03@gmail.com>
2026-09-25 14:28:50 +0500

    Complete research dashboard foundation: tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,1 @@
-export default {
-  darkMode: "class",
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: { fontFamily: { sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"] } } },
-  plugins: []
-};
+export default { darkMode:"class", content:["./index.html","./src/**/*.{js,jsx}"], theme:{extend:{fontFamily:{sans:["Inter","ui-sans-serif","system-ui","sans-serif"]}}}, plugins:[] };
