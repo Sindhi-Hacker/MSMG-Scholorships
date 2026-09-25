1329bc9f37854cc64a4540652e432a280c0a3860
Sindhi Hacker <sindhihacker03@gmail.com>
2026-09-25 14:51:26 +0500

    Fix runtime ReferenceError from comparison icon

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import {useState} from "react";
 import {NavLink,Outlet,useLocation} from "react-router-dom";
 import {FiAlertTriangle,FiBarChart2,FiBookOpen,FiBriefcase,FiCalendar,FiCheckCircle,FiDollarSign,FiFileText,FiGitBranch,FiGrid,FiLayers,FiMap,FiMenu,FiSearch,FiShield,FiSliders,FiTarget,FiX,FiSun,FiMoon} from "react-icons/fi";
 const groups=[
-["Research",[[ "/","Dashboard",FiGrid],["/scholarships","Scholarship Explorer",FiSearch],["/programs","Master's Programs",FiBookOpen],["/top-10","Top 10 Opportunities",FiTarget],["/comparison","Funding Comparison",FiGitCompare]]],
+["Research",[[ "/","Dashboard",FiGrid],["/scholarships","Scholarship Explorer",FiSearch],["/programs","Master's Programs",FiBookOpen],["/top-10","Top 10 Opportunities",FiTarget],["/comparison","Funding Comparison",FiGitBranch]]],
 ["Funding",[[ "/funding/need-based","Need-Based Funding",FiShield],["/funding/fully-funded","Fully Funded",FiCheckCircle],["/funding/tuition-free","Tuition-Free Options",FiLayers],["/funding/low-cost","Low-Cost Backups",FiDollarSign],["/deadlines","Deadline Calendar",FiCalendar]]],
 ["Explore",[[ "/countries","Country Explorer",FiMap],["/regions","Region Explorer",FiLayers],["/fields","Program / Field Explorer",FiBriefcase]]],
 ["Planning",[[ "/eligibility","Eligibility Checker",FiSliders],["/budget","Budget Calculator",FiBarChart2],["/documents","Document Checklist",FiFileText],["/roadmap","Application Roadmap",FiCalendar],["/do-not-waste-time","Do Not Waste Time On",FiAlertTriangle]]]
